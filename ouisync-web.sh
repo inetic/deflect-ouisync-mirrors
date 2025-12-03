a2da49dfe1f183f21961af1bb69e3d2031923235
inetic <40127+inetic@users.noreply.github.com>
2025-12-03 16:05:37 +0100

    Refactor to mirror directories only
    That is:
    * Removed nginx serving
    * Periodically rsync data into repository
    * Periodically rsync data out of repository

--- ouisync-web.sh
+++ ouisync-web.sh
@@ -3,81 +3,43 @@
 set -e
 
 ######################################################################
-http_port=8080
-default_container_name=ouisync-web
+default_container_name=ouisync-mirrors
 
-function print_usage() {
-    echo "Usage: $(basename $0) [--host host] [--container-name name] [--get-token access] [--start] [--create] [--import token] [--upload dir] [--serve]"
+function print_help() {
+    echo "Utility to mirror directories using Ouisync"
+    echo
+    echo "Usage: $(basename $0) [--host host] [--container-name name] ([--get-token ...] | [--primary ...] | [--mirror ...])"
+    echo
     echo "Options:"
-    echo "  --host host              IP or ~/.ssh/config entry of a server running docker where the commands shall run"
-    echo "  --container-name name    Name of the docker container where to perform commands. Defaults to $default_container_name"
-    echo "  --start                  Start the container and Ouisync inside it"
-    echo "  --create                 Create a new repository"
-    echo "  --upload dir             Upload content of dir into the repository"
-    echo "  --get-token acces        Get access token of a previously created repository. Must be 'blind','read' or 'write'"
-    echo "  --import token           Import an existing repository"
-    echo "  --serve                  Start serving content of the repository over http on port $http_port"
+    echo "  --host <HOST>"
+    echo
+    echo "      IP or ~/.ssh/config entry of a server running docker where the commands shall run"
+    echo
+    echo "  --container-name <NAME>"
+    echo
+    echo "      Name of the docker container where to perform commands. Defaults to $default_container_name"
+    echo
+    echo "  --primary <STORE> <IN_DIR>"
+    echo
+    echo "      Makes this script act as a \"primary\" server, meaning that content of <IN_DIR> will"
+    echo "      be mirrored into \"mirror\" servers. <STOREDIR> needs to point to a directory"
+    echo "      where ouisync will store the repository databases."
+    echo
+    echo "  --mirror <TOKEN> <OUT_DIR>"
+    echo
+    echo "      Makes this script act as a \"mirror\" server, meaning that content of a repository"
+    echo "      represented by <TOKEN> will be mirrored into the <OUT_DIR> directory."
 }
 
 function error() {
     echo "Error: $@"
     echo ""
-    print_usage
+    print_help
     exit 1
 }
 
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -h|--help)
-            echo "Script for serving web site shared over Ouisync"
-            echo ""
-            print_usage
-            exit
-            ;;
-        --host)
-            host=$2; shift
-            if [ -z "$host" ]; then
-                error "--host must not be empty"
-            fi
-            docker_host="--host ssh://$host"
-            ;;
-        --container-name)
-            container_name=$2; shift
-            if [ -z "$container_name" ]; then
-                error "--container-name must not be empty"
-            fi
-            ;;
-        --get-token)
-            do_get_token="yes"
-            get_token_type=$2; shift
-            if [ "$get_token_type" != "blind" -a "$get_token_type" != "read" -a "$get_token_type" != "write" ]; then
-                error "Invalid value for --get-token"
-            fi
-            ;;
-        --start) do_start="yes" ;;
-        --create) do_create="yes" ;;
-        --import)
-            do_import="yes"
-            import_token=$2; shift
-            if [[ ! "$import_token" =~ ^https://ouisync.net/r# ]]; then
-                error "--import requires a valid token (got '$import_token')"
-            fi
-            ;;
-        --upload)
-            do_upload="yes"
-            upload_src_dir=$2; shift
-            if [ ! -d "$upload_src_dir" ]; then
-                error "--upload requires a valid directory (got '$upload_src_dir')"
-            fi
-            ;;
-        --serve) do_serve="yes" ;;
-        *) error "Unknown argument: $1" ;;
-    esac
-    shift
-done
-
 container_name=${container_name:=$default_container_name}
-image_name=ouisync-web
+image_name=ouisync-mirrors
 repo_name=www
 
 ######################################################################
@@ -94,6 +56,10 @@ function exe_i() {
     dock exec -i $container_name "$@"
 }
 
+function enter(
+    dock exec -it $container_name bash
+)
+
 function enable_repo_defaults() {
     repo_name=$1
     # Tell ouisync where to mount repositories
@@ -108,9 +74,7 @@ function enable_repo_defaults() {
     exe ouisync local-discovery true
 }
 
-######################################################################
-# Start the container and ouisync inside it
-if [ "$do_start" = "yes" ]; then
+function run_container_detached (
     dock build -t $image_name - < ./Dockerfile
     
     # Flags to allow mounting inside the container
@@ -120,10 +84,42 @@ if [ "$do_start" = "yes" ]; then
     dock run \
         --detach \
         --net=host \
+        "$@" \
         $fuse_mounting_flags \
         --name $container_name $image_name \
         sleep infinity
+)
 
+function start_primary_container (
+    local store_dir=$1
+    local watch_dir=$2
+
+    if [ ! -d "$store_dir" ]; then
+        error "Store dir is not a valid directory ($store_dir)"
+    fi
+
+    if [ ! -d "$watch_dir" ]; then
+        error "Watch dir is not a valid directory ($watch_dir)"
+    fi
+
+    run_container_detached \
+        -v $store_dir:/opt/.local/share/ouisync \
+        -v $watch_dir:/watch_dir:ro
+)
+
+function start_mirror_container (
+    local out_dir=$1
+
+    # TODO: Check if token is valid
+
+    if [ ! -d "$out_dir" ]; then
+        error "Out dir is not a valid directory ($out_dir)"
+    fi
+
+    run_container_detached -v $out_dir:/out_dir:rw
+)
+
+function start_ouisync (
     # Start ouisync in the background
     exe sh -c 'nohup ouisync start &'
 
@@ -132,68 +128,117 @@ if [ "$do_start" = "yes" ]; then
 
     # Bind ouisync to IPv4 and IPv6 on random ports
     exe ouisync bind quic/0.0.0.0:0 quic/[::]:0
-fi
-
-######################################################################
-# Create the "www" repository
-if [ "$do_create" = "yes" ]; then
-    exe ouisync create $repo_name
-    enable_repo_defaults $repo_name
-fi
-
-######################################################################
-# Import repository from a token
-if [ "$do_import" = "yes" ]; then
-    exe ouisync create $repo_name --token $import_token
-    enable_repo_defaults $repo_name
-fi
-
-######################################################################
-# Get repository token 
-if [ "$do_get_token" = "yes" ]; then
-    exe ouisync share $repo_name --mode $get_token_type
-fi
-
-######################################################################
-# Upload content into the $repo_name repository
-if [ "$do_upload" = "yes" ]; then
-    # Append '/' to `$upload_src_dir`, otherwise rsync would copy the directory
-    # itself as opposed to its content.
-    if [ "${upload_src_dir: -1}" != "/" ]; then
-        upload_src_dir="$upload_src_dir/"
-    fi
-
-    # Ouisync doesn't support file/dir timestamps yet, so we can't rely
-    # on those.
-    no_timestamp_flags="--checksum --ignore-times"
-
-    rsync -e "docker $docker_host exec -i" -rv $no_timestamp_flags \
-        $upload_src_dir \
-        $container_name:/opt/ouisync/$repo_name
-fi
-
-######################################################################
-# Serve content of the repo
-
-nginx_config=$(cat << EOM
-user root;
-
-events {
-	worker_connections 768;
-}
-
-http {
-    server {
-        listen $http_port;
-        location / {
-            root /opt/ouisync/$repo_name;
-        }
-    }
-}
-EOM
 )
 
-if [ "$do_serve" = "yes" ]; then
-    echo "$nginx_config" | exe_i dd of=/etc/nginx/nginx.conf
-    exe nginx
-fi
+# Continuously `rsync` from `WATCH_DIR` into the repo
+function start_watching (
+    # TODO: Use something like lsyncd
+    local script=(
+        "while true; do"
+        "  rsync -rv --del --checksum --ignore-times /watch_dir/ ~/ouisync/www;"
+        "  sleep 1;"
+        "done"
+    )
+    exe bash -c "${script[*]}&"
+)
+
+# Continuously `rsync` from the repo into `OUT_DIR`
+function start_updating (
+    # TODO: Use something like lsyncd
+    local script=(
+        "while true; do"
+        "  rsync -rv --del --checksum --ignore-times ~/ouisync/www/ /out_dir;"
+        "  sleep 1;"
+        "done"
+    )
+    exe bash -c "${script[*]}&"
+)
+
+function is_valid_token (
+    local token=$1
+    if [[ ! "$token" =~ ^https://ouisync.net/r# ]]; then
+        return 1
+    fi
+)
+
+function import_ouisync_repo (
+    local token=$1
+    if ! is_valid_token "$token"; then
+        error "The token is invalid ($token)"
+    fi
+    exe ouisync create $repo_name --token $token
+    enable_repo_defaults $repo_name
+)
+
+function get_repo_token (
+    local token_type=$1
+    if [ "$token_type" != "blind" -a "$token_type" != "read" -a "$token_type" != "write" ]; then
+        error "Invalid value for --get-token ($token_type). Valid values are 'blind', 'read' or 'write'."
+    fi
+    exe ouisync share $repo_name --mode $token_type
+)
+
+function create_repo_if_doesnt_exist (
+    if [ -z "$(exe ls /opt/.local/share/ouisync/$repo_name.ouisyncdb)" ]; then
+        exe ouisync create $repo_name
+    else
+        echo "Repo already exists, reusing."
+    fi
+    enable_repo_defaults $repo_name
+)
+
+function act_as_primary (
+    store_dir=$1;
+    watch_dir=$2;
+    start_primary_container $store_dir $watch_dir
+    start_ouisync
+    create_repo_if_doesnt_exist
+    start_watching $watch_dir
+)
+
+function act_as_mirror (
+    token=$1;
+    out_dir=$2;
+    start_mirror_container $out_dir
+    start_ouisync
+    import_ouisync_repo $token
+    start_updating
+)
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            print_help
+            exit
+            ;;
+        --host|-H)
+            host=$2; shift
+            if [ -z "$host" ]; then
+                error "--host must not be empty"
+            fi
+            docker_host="--host ssh://$host"
+            ;;
+        --container-name|-c)
+            container_name=$2; shift
+            if [ -z "$container_name" ]; then
+                error "--container-name must not be empty"
+            fi
+            ;;
+        --primary|-p)
+            store_dir=$2; shift
+            watch_dir=$2; shift
+            act_as_primary $store_dir $watch_dir
+            ;;
+        --mirror|-m)
+            token=$2; shift
+            out_dir=$2; shift
+            act_as_mirror $token $out_dir
+            ;;
+        --get-token)
+            token_type=$2; shift
+            get_repo_token $token_type
+            ;;
+        *) error "Unknown argument: $1" ;;
+    esac
+    shift
+done
